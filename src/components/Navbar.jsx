9c7cf91895b2cb5eb30d08428c0fc1b47134dea4
developer-mmm <ahmadahmadaliyev7@gmail.com>
2024-06-30 23:19:23 +0500

    changed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,22 @@
 import React from "react";
+// redux
 import { useSelector } from "react-redux";
+
+//firebase;
+import { auth } from "../firebase/firebiseConfig";
+import { signOut } from "firebase/auth";
+import toast from "react-hot-toast";
 
 function Navbar() {
   const { user } = useSelector((state) => state.user);
+  const handleoOut = async () => {
+    try {
+      await signOut(auth);
+      toast.success("back again ✋");
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
   return (
     <header className="bg-base-200">
       <nav className="navbar align-elements">
@@ -13,18 +27,26 @@ function Navbar() {
         </div>
         <div className="navbar-center">contact</div>
         <div className="navbar-end">
-          <div className="flex">
-            <h4 className="text-red-600 border-2 p-2 border-blue-400 rounded-full mr-4">{user.email}</h4>
+          <div className="flex items-center gap-4">
+            <h4 className="text-red-600  border-2 p-2 border-blue-400 rounded-full mr-4">
+              {user.email}
+            </h4>
+            <div className="avatar">
+            <div className="ring-primary mr-4 ring-offset-base-100 w-10 h-10 rounded-full ring ring-offset-2">
             <img
               src={
-                user.photoURL
+                user.displayName
                   ? user.photoURL
-                  : `https://api.dicebear.com/9.x/initials/svg?seed=${user.photoURL}`
+                  : `https://api.dicebear.com/9.x/initials/svg?seed=${user.displayName}`
               }
               alt=""
             />
+            </div>
           </div>
-          <button className="btn btn-secondary">EXIT</button>
+          </div>
+          <button onClick={handleoOut} className="btn btn-secondary">
+            EXIT
+          </button>
         </div>
       </nav>
     </header>
