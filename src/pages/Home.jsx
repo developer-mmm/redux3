4f8e66513d06c391465bb361a54dad148048f6f6
developer-mmm <ahmadahmadaliyev7@gmail.com>
2024-07-03 06:12:14 +0500

    changed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,9 +2,10 @@ import { useSelector } from "react-redux";
 import { useCollection } from "../hooks/useCollection";
 import { Form, useActionData } from "react-router-dom";
 import { Checkbox, FormInput } from "../components";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { collection, addDoc, doc, deleteDoc } from "firebase/firestore";
 import { db } from "../firebase/firebiseConfig";
+import toast from "react-hot-toast";
 
 export const action = async ({ request }) => {
   let formData = await request.formData();
@@ -18,19 +19,32 @@ function Home() {
   const { user } = useSelector((state) => state.user);
   const { data: todos } = useCollection("todos", ["uid", "==", user.uid]);
   const userData = useActionData();
-
+  const [errors, setErrors] = useState({
+   
+  });
   useEffect(() => {
     if(userData){
       const NewDoc = {
         ...userData,
         uid: user.uid,
       };
-      addDoc(collection(db, "todos"), NewDoc)
+
+      if (!userData?.title.trim()) {
+        setErrors((prev) => {
+          return {...prev, title: "input-error"}
+        })
+      }
+
+      addDoc(collection(db, "todos"), NewDoc).then(()=> {
+        toast.success("Added")
+      })
     }
   }, [userData])
 
   const deleteDocument = (id) => {
-    deleteDoc(doc(db, "todos", id))
+    deleteDoc(doc(db, "todos", id)).then(()=> {
+      toast.success("deleted")
+    })
   }
   return (
     <div className="align-elements ">
@@ -39,7 +53,7 @@ function Home() {
           {todos &&
             todos.map((todo) => {
               return (
-                <div key={todo.id} className="flex g-4 items-center border-2 rounded-2xl w-96 justify-between p-5 mt-2">
+                <div key={todo.id} className="flex g-4 items-center border-2 rounded-2xl w-96 justify-between p-5 shadow-xl mt-2">
                   <h3 className="text-3xl">{todo.title}</h3>
                   <button onClick={() => deleteDocument(todo.id)} className="btn btn-primary btn-sm">Delete</button>
                 </div>
@@ -52,10 +66,10 @@ function Home() {
             className="flex flex-col items-center gap-5 card bg-base-100 w-96 p-5 shadow-xl"
           >
             <h1>Add New Todo</h1>
-            <FormInput type="text" labelText="title" name="title" />
+            <FormInput type="text" labelText="title" name="title" status={errors.title} />
             <Checkbox name="completed" />
             <div className="w-full">
-              <button className="btn btn-secondary btn-block">Add</button>
+              <button  className="btn btn-secondary btn-block">Add </button>
             </div>
           </Form>
         </div>
